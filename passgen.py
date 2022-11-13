be6f0fbda499eb5acea9bb7607313976f99f2d9e
x1KTO <116457286+x1KTO@users.noreply.github.com>
2022-11-14 03:53:41 +0600

    Main password generator script
    Main password generator script. It is the bare minimum.

---
+++ passgen.py
@@ -0,0 +1,17 @@
+import random
+
+ch = "abcdefghijklmnopqrstABCDEFGHIJKLMNOPQRST1234567890@#$%"
+
+while 1:
+    pl = int(input("How many character? : ")) 
+    pn= int(input("how many variation? : ")) 
+    for x in range(0,pn):
+
+        p = ""
+
+        for x in range(0,pl): 
+            pc = random.choice(ch)
+
+            p = p + pc
+
+        print("Here is your strong password : ", p)
